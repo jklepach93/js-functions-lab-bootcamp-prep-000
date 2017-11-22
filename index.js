815b9ef86723372672a8269fb86f45bd2614ba49
Justin Klepach <justin.klepach93@gmail.com>
2017-11-22 18:00:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // write your code below!
+function happyHolidays(string) {
+  return "Happy holidays!"
+}
+function happyHolidaysTo(firstName) {
+console.log('Happy holidays, ${name}!')
+happyHolidaysTo("Justin") // "Happy holidays, Justin!"
+}
