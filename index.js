40aabc7843563c3334856d93922c95e76a33b224
Justin Klepach <justin.klepach93@gmail.com>
2017-11-22 18:05:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,6 @@ return `Happy holidays, ${name}!`;
 function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`;
 } ;
+function holidayCountdown(holiday, number) {
+return `It\'s ${number} days until ${holiday}!`;
+} ;
